16ae5edf2860c7ed0887a3baf8640fa2010bf327
Jeiel Tarazona Lovera <jeiel.disas@gmail.com>
2021-01-14 08:56:29 -0500

    Agregado pagination response

---
+++ back-end/Proyecto.Domain/Models/Response/PagedResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto.Domain.Models.Response
+{
+    public class PagedResponse<T>
+    {
+		public IEnumerable<T> Data { get; set; }
+		public int? PageNumber { get; set; }
+		public int? PageSize { get; set; }
+		public int? MaxPage { get; set; }
+		public int? Total { get; set; }
+		public string NextPage { get; set; }
+		public string PreviousPage { get; set; }
+		
+		public PagedResponse()
+        {
+
+        }
+
+		public PagedResponse(IEnumerable<T> data)
+		{
+			Data = data;
+		}
+	}
+}
--- back-end/Proyecto.Domain/Services/Querys/IGrupoGraduadoServiceQuery.cs
+++ back-end/Proyecto.Domain/Services/Querys/IGrupoGraduadoServiceQuery.cs
@@ -1,4 +1,5 @@
-﻿using Proyecto.Domain.Models.Response;
+﻿using Proyecto.Domain.Models.Request;
+using Proyecto.Domain.Models.Response;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,5 +9,7 @@ namespace Proyecto.Domain.Services.Querys
     public interface IGrupoGraduadoServiceQuery : IServiceQuery<GrupoGraduadoResponse>
     {
         public Task<IEnumerable<object>> GetSumGraduadosByCursoAndYear(DateTime date);
+
+        public Task<PagedResponse<GrupoGraduadoResponse>> GetAllPaged(PaginationFilter paginationFilter);
     }
 }
